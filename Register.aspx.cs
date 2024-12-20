f902b9c0004e1b907c8a8c0c5a9fc2642446b093
Fenilsheta <139640320+Fenilsheta@users.noreply.github.com>
2024-12-20 19:32:18 +0530

    database

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace college_work
 {
     public partial class Register : System.Web.UI.Page
     {
+        SqlConnection conn;
+        protected void fnConnection()
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["Mydatabase"].ConnectionString;
+            conn = new SqlConnection(strcon);
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+                Response.Write("Connection Successed");
+            }
+            else
+            {
+                Response.Write("not connection");
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            fnConnection();
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
